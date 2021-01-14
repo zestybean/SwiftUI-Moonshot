dfa69894e7984cb35c185acb8844ae4ca2a2ac12
Brando <brando.lugo@gmail.com>
2021-01-13 19:28:16 -0600

    Initial Commit

---
+++ Shared/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
